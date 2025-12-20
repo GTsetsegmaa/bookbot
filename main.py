cfcbcb2e24bbc495bfa3f9a5e0840fd5d95d0f62
GTsetsegmaa <gtsetsegm@gmail.com>
2025-12-20 10:19:33 -0800

    add sys to get filepath from user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, character_count, sorted_dict
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,16 +7,22 @@ def get_book_text(path_to_file):
         return file_contents
 
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
-    num_words = count_words(frankenstein)
-    num_of_chars = character_count(frankenstein)
+    try:
+        file_path = sys.argv[1]
+    except Exception:
+        print("Usage: python3 main.py <path_to_book>")
+
+    book = get_book_text(file_path)
+    num_words = count_words(book)
+    num_of_chars = character_count(book)
     sorted_chars = sorted_dict(num_of_chars)
     
-    print("============= BOOKBOT =============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("\n============= BOOKBOT =============")
+    print(f"Analyzing book found at {file_path}...")
     print(f"Found {num_words} total words")
     print("---------- Character Count -------")
     for diction in sorted_chars:
         if diction['char'].isalpha():
             print(f"{diction['char']}: {diction['num']}")
+    print("=============== End ===============")
 main()
