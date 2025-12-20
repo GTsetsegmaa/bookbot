b818d2072093a1ad05331bc1a8a7e3cbef9cf422
GTsetsegmaa <gtsetsegm@gmail.com>
2025-12-20 10:04:26 -0800

    add printing for bookbot

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ def get_book_text(path_to_file):
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
     num_words = count_words(frankenstein)
-    print(f"Found {num_words} total words")
     num_of_chars = character_count(frankenstein)
     sorted_chars = sorted_dict(num_of_chars)
     
-    print(sorted_chars)
-
+    print("============= BOOKBOT =============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Found {num_words} total words")
+    print("---------- Character Count -------")
+    for diction in sorted_chars:
+        if diction['char'].isalpha():
+            print(f"{diction['char']}: {diction['num']}")
 main()
